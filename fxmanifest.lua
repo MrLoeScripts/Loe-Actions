594930282e9a2eafef65bd2d4b43fa5bf8cdff12
Mr. Loe <157546293+MrLoescripts@users.noreply.github.com>
2024-11-23 06:50:29 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'MrLoeScripts'
+description 'Carrying, Piggyback & Hostage Script for Serious RP'
+version '1.0'
+
+client_scripts {
+    'client.lua' 
+}
+
+server_scripts {
+    'server.lua' 
+}
+
+
